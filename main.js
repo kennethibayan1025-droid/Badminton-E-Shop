000226af13922498dc32d0a36858b94dbc963da9
Kenneth Ibayan <kennethibayan1025@gmail.com>
2025-11-10 23:58:30 +0800

    Better Product Pages

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// -------------------- Nav Area --------------------
 // SIDEBAR
 function showSidebar (){
   const sideBar = document.querySelector(".sideBar");
@@ -9,7 +10,8 @@ function closeSidebar (){
   sideBar.style.display = "none";
 }
 
-
+// -------------------- Main Area --------------------
+// HOME AREA
 // SLIDESHOWS
 let index = 0;
 const slides = document.querySelectorAll('.slide');
@@ -34,3 +36,25 @@ document.querySelector('.next').addEventListener('click', () => {
 setInterval(() => {
   showSlide(index + 1);
 }, 10000);
+
+// PRODUCTS AREA
+// SORT AND SEARCH FUNCTION
+
+// PRODUCTS DISPLAY
+
+// PAGINATION FUNCTION
+const totalPages = document.querySelectorAll('.products').length;
+const container = document.querySelector('.pageButtons');
+let pageIndex = 1;
+
+for (let i = 1; i <= totalPages; i++) {
+  const button = document.createElement('button');
+  button.textContent = `${i}`;
+  button.classList.add('page');
+
+  if (i === pageIndex) {
+    button.classList.add('active');
+  }
+
+  container.appendChild(button);
+}
